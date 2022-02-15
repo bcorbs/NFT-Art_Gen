16643e6461e92605ca5124e213c2a3292b917e68
bcorbs <95234603+bcorbs@users.noreply.github.com>
2022-02-15 14:45:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ traits = [
 ] # The different layers and the order that they will be used - MUST be same as trait layer folders
 imageCount = 1 # Total number of images to create
 nameFormat = "NFT #[NUMBER]" # The name of each NFT - '[NUMBER]' will be replaced with the NFT number
-description = "Really awesome description!" # Description of collection
+description = "" # Description of collection
 royalty = 2.5 # Royalty percentage
-royaltyAddress = "6R1YMnnjWsj93a438whWkimCimNzAgKRid7QkUfpkCxF" # Address to pay the royalties to
-collectionName = "Cool collection" # Name of collection
-collectionFamily = "Cool collection" # Name of family (often same as collection)
+royaltyAddress = "" # Address to pay the royalties to
+collectionName = "" # Name of collection
+collectionFamily = "" # Name of family (often same as collection)
 symbol = "" # Symbol (often blank)
 blockchain = "ETH" # ETH or SOL
